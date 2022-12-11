ce41a1b2e833eee485426285dc2a3717645a637a
Nicolas Del Piano <ndel314@gmail.com>
2022-12-10 23:35:08 -0300

    Solution to day 9 first puzzle

--- 2022/day-9/main.ts
+++ 2022/day-9/main.ts
@@ -1,13 +1,14 @@
 import * as fs from 'node:fs'
 import { parseHeadRopeMoves } from './moves'
-import { startSimulation } from './simulation'
+import { startSimulation, visitedTailPositions } from './simulation'
 
 function main() {
   const inputfile = process.argv[2] || 'input.txt'
   const data = fs.readFileSync(inputfile, { encoding: 'utf-8' })
 
   const headRopeMoves = parseHeadRopeMoves(data)
-  console.log(startSimulation(headRopeMoves))
+  const simulation = startSimulation(headRopeMoves)
+  console.log(visitedTailPositions(simulation).size)
 }
 
 main()
--- 2022/day-9/simulation.ts
+++ 2022/day-9/simulation.ts
@@ -27,7 +27,131 @@ type Simulation = {
 
 function simulate(s: Simulation): Simulation {
   // TODO: calculate simulation
+
+  for (const m of s.headMoves) {
+    for (let i = 0; i < m.n; i++) {
+      switch (m.move) {
+        case 'down':
+          {
+            const currentRopePos = s.simulation[s.simulation.length - 1]
+            const newHeadPos = {
+              ...currentRopePos.head,
+              y: currentRopePos.head.y + 1,
+            }
+
+            let newTailPos = { ...currentRopePos.head }
+
+            if (touch(newHeadPos, currentRopePos.tail)) {
+              newTailPos = { ...currentRopePos.tail }
+            }
+
+            s.simulation = [
+              ...s.simulation,
+              {
+                head: newHeadPos,
+                tail: newTailPos,
+              },
+            ]
+          }
+          break
+        case 'up':
+          {
+            const currentRopePos = s.simulation[s.simulation.length - 1]
+            const newHeadPos = {
+              ...currentRopePos.head,
+              y: currentRopePos.head.y - 1,
+            }
+
+            let newTailPos = { ...currentRopePos.head }
+
+            if (touch(newHeadPos, currentRopePos.tail)) {
+              newTailPos = { ...currentRopePos.tail }
+            }
+
+            s.simulation = [
+              ...s.simulation,
+              {
+                head: newHeadPos,
+                tail: newTailPos,
+              },
+            ]
+          }
+          break
+        case 'right':
+          {
+            const currentRopePos = s.simulation[s.simulation.length - 1]
+            const newHeadPos = {
+              ...currentRopePos.head,
+              x: currentRopePos.head.x + 1,
+            }
+
+            let newTailPos = { ...currentRopePos.head }
+
+            if (touch(newHeadPos, currentRopePos.tail)) {
+              newTailPos = { ...currentRopePos.tail }
+            }
+
+            s.simulation = [
+              ...s.simulation,
+              {
+                head: newHeadPos,
+                tail: newTailPos,
+              },
+            ]
+          }
+          break
+        case 'left':
+          {
+            const currentRopePos = s.simulation[s.simulation.length - 1]
+            const newHeadPos = {
+              ...currentRopePos.head,
+              x: currentRopePos.head.x - 1,
+            }
+
+            let newTailPos = { ...currentRopePos.head }
+
+            if (touch(newHeadPos, currentRopePos.tail)) {
+              newTailPos = { ...currentRopePos.tail }
+            }
+
+            s.simulation = [
+              ...s.simulation,
+              {
+                head: newHeadPos,
+                tail: newTailPos,
+              },
+            ]
+          }
+          break
+      }
+    }
+  }
+
   return s
+}
+
+type Grid = string[][]
+
+function printSimulation(s: Simulation): Grid {
+  const grid: Grid = []
+
+  // TODO: print sim
+
+  return grid
+}
+
+export function visitedTailPositions(s: Simulation): Set<string> {
+  const visited = new Set<string>()
+
+  for (const m of s.simulation) {
+    visited.add(`${m.tail.x}-${m.tail.y}`)
+  }
+
+  return visited
+}
+
+function touch(head: Position, tail: Position): boolean {
+  return Math.abs(head.x - tail.x) < 2 && Math.abs(head.y - tail.y) < 2
 }
 
 export function startSimulation(headRopeMoves: HeadRopeMoves): Simulation {
